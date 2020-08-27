0585e2d8ab95ede78f3229bd3f61ba741aa4d842
Tharun sekar <tharunsd23@gmail.com>
2020-08-27 17:42:14 +0530

    Updates

---
+++ precache-manifest.384387322ba300db3c04971e849c65ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead3ebdcb53d79a0966163f37d42e630",
+    "url": "/alan-bi/index.html"
+  },
+  {
+    "revision": "54139d511f0ce144ad21",
+    "url": "/alan-bi/static/css/main.7f80841f.chunk.css"
+  },
+  {
+    "revision": "cea98c5083043780cc0c",
+    "url": "/alan-bi/static/js/2.45d63fac.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/alan-bi/static/js/2.45d63fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54139d511f0ce144ad21",
+    "url": "/alan-bi/static/js/main.7d682a5c.chunk.js"
+  },
+  {
+    "revision": "00e40d12f3ca525f2946",
+    "url": "/alan-bi/static/js/runtime-main.c9be1159.js"
+  }
+]);
